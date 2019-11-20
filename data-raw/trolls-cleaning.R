891e20f89506532cf318d54f34285e5af423add2
Tara Valladares <tls8vx@virginia.edu>
2019-11-20 16:12:45 -0500

    Changed how the trolls data is cleaned. rt's are now removed, as are http links and empty cells.

--- data-raw/trolls-cleaning.R
+++ data-raw/trolls-cleaning.R
@@ -1,7 +1,19 @@
 ## code to prepare `trolls` dataset
 
+### not run ###
+# trolls_raw <- vector("list", ndoc)
+# for(i in 1:ndoc){
+#   trolls_raw[[i]]<- read.csv(paste0("../russian-troll-tweets-master/IRAhandle_tweets_", i,".csv"),
+#               stringsAsFactors = FALSE)
+# }
+# trolls<-vector("list",ndoc)
+# for(i in 1:ndoc){
+#   trolls[[i]]<- trolls_raw[[i]][which(trolls_raw[[i]]$language == "English"),]
+# }
+# save(data=trolls, file="trolls")
+
+
 load("/Users/tara/Box/Troll Tweets/tt_individualmodels/trolls")
-removeRetweets <- function(x){gsub("^rt.*", "rt", x)} # remove reposts
 
 # Remove retweets
 trolls.clean <- lapply(trolls,function(x){
@@ -13,12 +25,14 @@ trolls.clean <- lapply(trolls.clean, function(y){
   content<- y$content
   content<- stringi::stri_trans_general(content, "latin-ascii")
   content<- gsub("&amp", "and", content)
+  content<- gsub("http[^[:space:]]*", "", content) # remove links
   content<- tolower(content)
-  content<- removeRetweets(content)
+  content<- gsub("^rt.*", "", content) # remove reposts
 
-  results <- cbind(content, y[,c("author", "publish_date", "followers", "updates","account_type")])
+  results<- cbind(content, y[,c("author", "publish_date", "followers", "updates","account_type")])
   results<- results[which(results$account_type=="Left"|
                             results$account_type=="Right"),]
+  results<- subset(results, content !="") #remove empty tweets
 })
 
 trolls<-do.call(rbind, trolls.clean)
